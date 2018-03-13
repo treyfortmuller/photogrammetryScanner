0f01d0a77aee865a6ac8527d90d17ae2370c6a2e
Trey Fortmuller <tfortmuller@mac.com>
2018-03-12 20:03:33 -0700

    wiped for tessellate

--- sfm_test/scratch_scripts/scratch_external_call.py
+++
@@ -1,8 +0,0 @@
-# Test the calling of a python script within a python script
-# Trey Fortmuller
-
-import os
-
-cwd = os.path.join(os.getcwd(), "scratch_external_called.py")
-
-os.system('{} {}'.format('python', cwd))
