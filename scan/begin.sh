2efe2c519efc3f216a5c8bc91e6b39d85c4c9df4
nathan-le <nathan.le@berkeley.edu>
2018-04-28 21:21:46 -0700

    updating installation requirements

--- scan/begin.sh
+++ scan/begin.sh
@@ -3,6 +3,10 @@
 
 # virtualenv -p python3 venv || python3 -m venv venv || python -m venv venv
 # source venv/bin/activate
-
+pip install --upgrade pip
 pip install --requirement requirements.txt
+pip install numpy
+pip install pyserial
+
+
 FLASK_APP=server.py flask run
